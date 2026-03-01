bbb603f64d2606aa93551b4e8f27509a297945c4
Ashka Pathak <pathak.ashka@gmail.com>
2026-03-01 16:32:14 +0530

    GA3 Q12: fix CORS for browser fetch (disable credentials)

--- GA3/q12_function_calling/main.py
+++ GA3/q12_function_calling/main.py
@@ -9,7 +9,8 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=Falseø
+,
     allow_methods=["*"],
     allow_headers=["*"],
 )
